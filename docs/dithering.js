478dad1d16f3d20f147dc77a9dc5c30a731dd81c
Jake <505e06b2@gmail.com>
2021-01-17 11:42:48 +0000

    HUGE REWRITE - massive performance gains, increased readability, and altered layout

--- docs/dithering.js
+++ docs/dithering.js
@@ -1,69 +1,62 @@
 // Credit to https://gist.github.com/PhearTheCeal/6443667 for the algorithm
 
-var image_data;
+function Dithering(canvas) {
+	const ctx = canvas.getContext("2d");
 
-function clip(x) {
-	return x < 0 ? 0 : (x > 255 ? 255 : x)
-}
+	this.canvas = canvas;
+	this.image_data = new Uint8Array(ctx.getImageData(0,0, canvas.width, canvas.height).data); //clone
 
-function setPixel(x, y, color) {
-	var index = (x + y * image_data.width) * 4;
-	image_data.data[index+0] = parseInt(color[0]+0.5);
-	image_data.data[index+1] = parseInt(color[1]+0.5);
-	image_data.data[index+2] = parseInt(color[2]+0.5);
-	image_data.data[index+3] = 255;
-}
+	let oldpixel;
+	let newpixel;
+	let quant_error;
+	let err_red, err_green, err_blue;
 
-function color_diff(one, two) {
-	return [(one[0] - two[0]), (one[1] - two[1]), (one[2] - two[2])];
-}
+	const _getPixel = (x, y) => {
+		const index = (x + y * canvas.width) * 4;
+		return [ this.image_data[index+0], this.image_data[index+1], this.image_data[index+2] ];
+	}
 
-function color_add_err(x, y, err_red, err_green, err_blue) {
-	var index = (x + y * image_data.width) * 4;
-	image_data.data[index+0] = clip(image_data.data[index+0]+err_red)
-	image_data.data[index+1] = clip(image_data.data[index+1]+err_green)
-	image_data.data[index+2] = clip(image_data.data[index+2]+err_blue)
-	image_data.data[index+3] = 255;
-}
+	const _setPixel = (x, y, colour) => {
+		const index = (x + y * canvas.width) * 4;
+		this.image_data[index+0] = Math.floor(colour[0]+0.5);
+		this.image_data[index+1] = Math.floor(colour[1]+0.5);
+		this.image_data[index+2] = Math.floor(colour[2]+0.5);
+		this.image_data[index+3] = 255;
+	}
 
-function find_closest_palette_color(pixel) {
-	return (0.2126*pixel[0] + 0.7152*pixel[1] + 0.0722*pixel[2]) > 128 ? [255,255,255] : [0,0,0];
-}
+	const _closestPalleteColour = (pixel) => {
+		return (0.2126*pixel[0] + 0.7152*pixel[1] + 0.0722*pixel[2]) > 128 ? [255,255,255] : [0,0,0];
+	}
 
-function get_pixel(x, y) {
-	var index = (x + y * image_data.width) * 4;
-	return [ image_data.data[index+0], image_data.data[index+1], image_data.data[index+2] ];
-}
+	const _colourDifference = (one, two) => {
+		return [(one[0] - two[0]), (one[1] - two[1]), (one[2] - two[2])];
+	}
 
-function rgb2bin() {
-	image_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	const _colourAddError = (x, y, err_red, err_green, err_blue) => {
+		const clip = (x) => (x < 0 ? 0 : (x > 255 ? 255 : x));
+		const index = (x + y * canvas.width) * 4;
+		this.image_data[index+0] = clip(this.image_data[index+0] + err_red)
+		this.image_data[index+1] = clip(this.image_data[index+1] + err_green)
+		this.image_data[index+2] = clip(this.image_data[index+2] + err_blue)
+		this.image_data[index+3] = 255;
+	}
 
-	var oldpixel;
-	var newpixel;
-	var quant_error;
-	var err_red, err_green, err_blue;
 
-	for (var y = 0; y < canvas.height; y++) {
-		for (var x = 0; x < canvas.width; x++) {
-			oldpixel = get_pixel(x, y);
-			newpixel = find_closest_palette_color(oldpixel);
-			setPixel(x, y, newpixel);
-			quant_error = color_diff(oldpixel, newpixel);
+	for(let y = 0; y < canvas.height; y++) {
+		for(let x = 0; x < canvas.width; x++) {
+			oldpixel = _getPixel(x, y);
+			newpixel = _closestPalleteColour(oldpixel);
+			_setPixel(x, y, newpixel);
+			quant_error = _colourDifference(oldpixel, newpixel);
 
 			err_red = quant_error[0];
 			err_green = quant_error[1];
 			err_blue = quant_error[2];
 
-			if (x+1 < canvas.width)
-				color_add_err(x+1, y,   (7/16) * err_red, (7/16) * err_green, (7/16) * err_blue);
-			if (x-1 > 0 && y+1 < canvas.height)
-				color_add_err(x-1, y+1, (3/16) * err_red, (3/16) * err_green, (3/16) * err_blue);
-			if (y+1 < canvas.height)
-				color_add_err(x,   y+1, (5/16) * err_red, (5/16) * err_green, (5/16) * err_blue);
-			if (x+1 < canvas.width)
-				color_add_err(x+1, y+1, (1/16) * err_red, (1/16) * err_green, (1/16) * err_blue);
+			if(x+1 < canvas.width)             _colourAddError(x+1, y,   (7/16) * err_red, (7/16) * err_green, (7/16) * err_blue);
+			if(x-1 > 0 && y+1 < canvas.height) _colourAddError(x-1, y+1, (3/16) * err_red, (3/16) * err_green, (3/16) * err_blue);
+			if(y+1 < canvas.height)            _colourAddError(x,   y+1, (5/16) * err_red, (5/16) * err_green, (5/16) * err_blue);
+			if(x+1 < canvas.width)             _colourAddError(x+1, y+1, (1/16) * err_red, (1/16) * err_green, (1/16) * err_blue);
 		}
 	}
-
-	ctx.putImageData(image_data, 0, 0);
 }
--- docs/main.js
+++ docs/main.js
@@ -1,100 +1,85 @@
-//seriously needs a re-write lol - this was originally made a little while before "let" was the standard
+const settings = {
+	last_canvas: null,
+	last_dithering: null,
+	last_source: "",
 
-var file = null;
-var text = {};
-var max_width = 50;
-var inverted = false;
-var dithering = false;
-var monospace = false; //use a blank - only reliable if spaced correctly though
-var canvas;
-var ctx;
-var charcount;
-
-var r = 1;
-var g = 1;
-var b = 1;
-
-window.onload = function() {
-	text = document.getElementById("text");
-	file = "select.png";
-	charcount = document.getElementById("charcount");
-	darkTheme(inverted);
-	filechanged();
+	width: 62,
+	inverted: false,
+	dithering: false,
+	monospace: false,
 }
 
-function get_char(current) {
-	//Codepoint reference - https://www.ssec.wisc.edu/~tomw/java/unicode.html#x2800
-	let codepoint_offset = 0;
-    if(current.every( (x) => (x === 0) )) { //all empty
-		codepoint_offset = (monospace) ? 0 : 4; //0x2800 is a blank braille char
-    } else {
-        codepoint_offset = (current[0] << 0) + (current[1] << 1) + (current[2] << 2) + (current[4] << 3) + (current[5] << 4) + (current[6] << 5) + (current[3] << 6) + (current[7] << 7);
+function setUIElement(selector, value) {
+	const elem = document.querySelector(selector);
+	switch(elem.getAttribute("type")) { //should all be <input>
+		case "checkbox":
+			elem.checked = value;
+			break;
+
+		default:
+			elem.value = value;
 	}
-    return String.fromCharCode(0x2800 + codepoint_offset);
+	return elem;
 }
 
-function nearest_multiple(num, mult) {
-    return num - (num % mult);
-}
-
-//events
-
-function tobraille(img) {
-	{ //place image on canvas and keep aspect ratio
-		var width = img.width;
-		var height = img.height;
-		if(img.width != (max_width * 2)) {
-			width = max_width * 2;
-			height = width * img.height / img.width;
-		}
-
-		canvas.width = nearest_multiple(width, 2);
-		canvas.height = nearest_multiple(height, 4);
+function initUI() {
+	document.body.ondragover = (e) => e.preventDefault();
+	document.body.ondrop = (e) => {
+		e.preventDefault();
+		loadNewImage(URL.createObjectURL(e.dataTransfer.items[0].getAsFile()));
+	}
+	document.body.onpaste = (e) => {
+		event.preventDefault();
+		loadNewImage(URL.createObjectURL(e.clipboardData.items[0].getAsFile()));
 	}
 
-	ctx = canvas.getContext("2d");
-	ctx.fillStyle="#FFFFFF"; //get rid of alpha
-	ctx.fillRect(0,0, canvas.width,canvas.height);
+	//buttons
+	const r = () => parseCanvas(settings.last_canvas); //shorten for compactness
 
-	ctx.mozImageSmoothingEnabled = false;
-	ctx.webkitImageSmoothingEnabled = false;
-	ctx.msImageSmoothingEnabled = false;
-	ctx.imageSmoothingEnabled = false;
-
-	ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-	if(dithering) rgb2bin();
-
-	var output_line = "";
-
-	for(var imgy = 0; imgy < canvas.height; imgy += 4) {
-		for(var imgx = 0; imgx < canvas.width; imgx += 2) {
-			var current = [0,0,0,0,0,0,0,0];
-			var cindex = 0;
-			for(var x = 0; x < 2; x++) {
-				for(var y = 0; y < 4; y++) {
-					var temp = ctx.getImageData(imgx+x, imgy+y, 1,1).data;
-					var avg = ((temp[0]/r) + (temp[1]/g) + (temp[2]/b)) / 3;
-					if(inverted) {
-						if(avg > 128) current[cindex] = 1;
-					} else {
-						if(avg < 128) current[cindex] = 1;
-					}
-					cindex++;
-				}
-			}
-			output_line += get_char(current);
-		}
-		output_line += "\n";
+	document.querySelector('input[type="file"]').onchange = (e) => {
+		 loadNewImage(URL.createObjectURL(e.target.files[0]));
 	}
-	text.value = output_line;
-	charcount.innerHTML = output_line.length;
+
+	setUIElement('#darktheme', settings.inverted).onchange = (e) => {
+		const element = document.querySelector('#text');
+		if(e.target.checked) element.classList.add("dark");
+		else element.classList.remove("dark");
+	};
+
+	setUIElement('#inverted', settings.inverted).onchange = (e) => {settings.inverted = e.target.checked; r()};
+	setUIElement('#dithering', settings.dithering).onchange = (e) => {settings.dithering = e.target.checked; r()};
+	setUIElement('#monospace', settings.monospace).onchange = (e) => {settings.monospace = e.target.checked; r()};
+
+	setUIElement('#width', settings.width).onchange = (e) => {
+		settings.width = e.target.value;
+		loadNewImage(settings.last_source);
+	};
+
+	document.querySelector('#clipboard').onclick = (e) => {
+		 document.querySelector('#text').select();
+		 document.execCommand("copy");
+	}
 }
 
-function filechanged(input) {
-	img = new Image();
-	canvas = document.createElement("CANVAS");
-	img.onload = function() {tobraille(img)};
-	if(input) file = URL.createObjectURL(input);
-	img.src = file;
+async function loadNewImage(src) {
+	if(src === undefined) return;
+
+	if(settings.last_source && settings.last_source !== src) URL.revokeObjectURL(settings.last_source);
+
+	settings.last_source = src;
+	const canvas = await createImageCanvas(src);
+	settings.last_canvas = canvas;
+	settings.last_dithering = null;
+	await parseCanvas(canvas);
+}
+
+async function parseCanvas(canvas) {
+	const text = canvasToText(canvas);
+	document.querySelector('#text').value = text;
+	document.querySelector('#charcount').innerText = text.length;
+}
+
+window.onload = () => {
+	initUI();
+	loadNewImage("select.png");
 }
